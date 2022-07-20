53b0da20b5c45eefd7464b7a9f96b6c881adb066
Hasbi-DOM <muhamad.hasbi.tif419@polban.ac.id>
2022-07-20 14:12:45 +0700

    push frontend

---
+++ frontend/src/components/RowTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <tr>
+    <td>{{ nim }}</td>
+    <td>{{ name }}</td>
+    <td>{{ gender }}</td>
+    <td>{{ department }}</td>
+    <td>{{ phone }}</td>
+    <td>{{ email }}</td>
+    </tr>
+
+</template>
+
+<script>
+    export const validationMessageCreators = {
+        required: () => 'Required',
+        nim: () => 'Invalid NIM',
+        txtMinLen: ({ $params }) => {
+            const min = plural(
+            'no characters | one character | {n} characters',
+            $params.txtMinLen.min
+            )
+            return `Must be at least ${min}`
+        }
+    }
+    export default {
+        props: ['nim','name','gender','department','phone','email'],
+        validate: function(dataItem) {
+
+        }
+    }
+    
+</script>
